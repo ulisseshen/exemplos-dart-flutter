5c123dc17dc8ddc59af824b07938c11c5864540d
Ulisses Hen <ulisses.hen@hotmail.com>
2022-01-07 14:28:07 -0300

    Create operadores_nullsafety.dart

---
+++ example/operadores_nullsafety.dart
@@ -0,0 +1,27 @@
+void main() {
+  String? olaa = null;
+  Teste? teste;
+
+  teste = Teste();
+  teste.algo();
+  teste = null;
+  try {
+    teste!.algo();
+    print('OK OK - teste!.algo(); não lançou exceção após Teste ser null');
+  } catch (e) {
+    print('ERROR - teste!.algo(); lançou exceção após Teste ser null');
+  }
+
+  try {
+    teste?.algo();
+    print('OK OK - teste?.algo(); não lançou exceção após Teste ser null');
+  } catch (e) {
+    print('ERROR - teste?.algo(); lançou exceção após Teste ser null');
+  }
+}
+
+class Teste {
+  algo() {
+    print('algo');
+  }
+}
